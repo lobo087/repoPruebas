5128220c3f7bbb883b51785961524886d4638e1b
lobo087 <ivreyesch@uide.edu.ec>
2025-10-04 20:40:04 -0500

    agregando lineas de conexion

--- proyectoNet/testing/Program.cs
+++ proyectoNet/testing/Program.cs
@@ -2,3 +2,8 @@
 Console.WriteLine("Hello, User!\nThis is a test");
 
 string string_api_key = "test_4acsdDFqjhQYU";
+
+string stripeApiKey = "asdQWE@dsd&$%&&/GFD%3rfghjde";
+
+string conn_url = "postgresql://developer:eemDpAEN.278@localhost:5432/testingbdd";
+
